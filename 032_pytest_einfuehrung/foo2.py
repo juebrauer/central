ae50e91e4791a0a583e458619155a283c5b1a118
juebrauer <juebrauer@gmail.com>
2022-09-10 09:05:40 +0200

    Adding a section that shows that several asserts or even none are possible in a single test function.

---
+++ 032_pytest_einfuehrung/foo2.py
@@ -0,0 +1,17 @@
+def foo(x,y):
+    return x+y
+
+def test1():
+    assert foo(2,3)==5
+    assert foo("ABC","DEF")=="ABCDEF"
+    assert 3==1+1
+    assert foo([1,2],[3,4])==[1,2,3,4]    
+
+def test2():
+    pass
+
+def test3():
+    return True
+
+def test4():
+    return False
